aab0afd5507706b3ee8b3411fd72ef2d7e8c3595
FilyanovichNP <nata.filyanovich@gmail.com>
2022-07-18 11:04:38 +0300

    done

--- src/run/Main.java
+++ src/run/Main.java
@@ -10,14 +10,14 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) throws FileNotFoundException {
-//        Scanner in = new Scanner(System.in);
-        System.out.println("Exchange rate: "+"1$= "+ Transfer.getDollar()+"p");
-        System.out.println("Exchange rate: "+"1p= $"+Transfer.getRuble());
-        System.out.println(Transfer.getDollarToRuble("$1"));
-        System.out.println(Transfer.getRubleToDollar("1000p"));
-//        System.out.print("Please, input string: ");
-//        Converter.readLine(in.nextLine());
+        Scanner in = new Scanner(System.in);
+        System.out.println("Exchange rate: " + "1$= " + Transfer.getDollar() + "p");
+        System.out.println("Exchange rate: " + "1p= $" + Transfer.getRuble());
+        System.out.print("Please, input string: ");
+        Converter.readLine(in.nextLine());
 
+        //System.out.println(Transfer.getDollarToRuble("$1"));
+        //System.out.println(Transfer.getRubleToDollar("1000p"));
 
     }
 }
